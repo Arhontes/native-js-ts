cde3d9e6fc7488ce1daec0736324300dd04bcf24
Arhontes <arhontes@yandex.ru>
2022-05-16 10:39:17 +0300

    refactoring

--- src/06/06-destructuring-assigment.test.ts
+++ src/06/06-destructuring-assigment.test.ts
@@ -1,25 +1,25 @@
 import {PropsType} from "./06-destructuring-assigment";
 
-let props:PropsType;
+let props: PropsType;
 
 
-beforeEach(()=>{
+beforeEach(() => {
     props = {
         name: 'Alex',
-        age:32,
-        lessons: [{title:'1',count: 1},{title:'2',count:2}],
+        age: 32,
+        lessons: [{title: '1', count: 1}, {title: '2', count: 2}],
         address: {
-            street:"Gagarina"
+            street: "Gagarina"
         }
     }
 })
-test("f1",()=>{
+test("f1", () => {
 
 
     //const age = props.age
 
     const {age, lessons} = props
-    const {age:vozrast, lessons:uroki} = props
+    const {age: vozrast, lessons: uroki} = props
 
     expect(age).toBe(32)
     expect(lessons.length).toBe(2)
@@ -28,11 +28,65 @@ test("f1",()=>{
 })
 
 
-test("f2",()=>{
+test("f2", () => {
     const [ag1, ag2] = props.lessons
-    const [,...restLesson]= props.lessons
+    const [, ...restLesson] = props.lessons
+    const [, , ...age] = props.lessons
     expect(ag1.count).toBe(1)
     expect(ag2.count).toBe(2)
     expect(restLesson.length).toBe(1)
+    expect(age).toStrictEqual([])
 
 })
+
+/*
+function toWeirdCase(string) {
+    let splitArray = string.split(" ");
+    let newArray = [];
+    for (let j = 0; j < splitArray.length; j++) {
+        let str = "";
+        for (let i = 0; i < splitArray[j].length; i++) {
+            if (i % 2) {
+                str += splitArray[j][i].toLowerCase()
+            } else {
+                str += splitArray[j][i].toUpperCase()
+            }
+        }
+        newArray.push(str)
+        str = ''
+    }
+    console.log(newArray.join(' '))
+    return newArray.join(' ');
+}
+*/
+
+// toWeirdCase("This is my codewars kata")
+/*
+
+const getString = (string: string) => {
+    return string.split(' ')
+        .map((word) => {
+            return word.split('')
+                .map((letter, index) => {return  index % 2 ? letter.toLowerCase() : letter.toUpperCase()
+                })
+                .join('')
+        }).join(' ')
+}
+getString('I am the best student')
+*/
+/*
+function twoSum(numbers:number[], target:number) {
+
+    let targetArr=[];
+    for (let i = 0; i < numbers.length; i++) {
+        for (let j = 1; j < numbers.length; j++) {
+            if(numbers[i]+numbers[j]===target){
+                targetArr.push(i)
+                targetArr.push(j)
+                return targetArr
+            }
+
+        }
+    }
+
+}*/
--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,22 @@ import './App.css';
 
 
 function App() {
+  function checkScope() {
+
+    const i = 'function scope';
+    for(;;){
+      const i = "dfdfd"
+    }
+
+    if (true) {
+      let i = 'block scope';
+      console.log('Block scope i is: ', i);
+    }
+
+    console.log('Function scope i is: ', i);
+    return i;
+  }
+  checkScope()
   return (
     <div className="App">
 
