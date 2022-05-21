b0688b9407f63208955b2166be89427fa207aee8
Arhontes <arhontes@yandex.ru>
2022-05-21 17:37:38 +0300

    Numeric conversion v2

--- src/MyTests/David_Flenagan/Numbers/numbers.test.ts
+++ src/MyTests/David_Flenagan/Numbers/numbers.test.ts
@@ -25,16 +25,52 @@ test("NaN test",()=>{
 
 test("number should be correct",()=>{
 
+   /*------------------------------- Как привести строку к числу----------------------------------- */
+
+    //парсим строку в которой первый четыре символа числа получается распарсить из в number
     const a = Number.parseFloat("63.24sometexthere")
     expect(a).toBe(63.24)
+    //приводим строку к числу с помощью +
     const b  = + "64"
     expect(b).toBe(64)
-    const d = Number(true)
+    //приводим строку к числу с помощью + , но здесть не получается и результат выражения NaN
+    const altB = +"64text"
+    expect(altB).toBe(NaN)
+    //a здесь получается с помощью Number т.к нет других символов
+    const altA = Number("121")
+    expect(altA).toBe(121)
+
+    /*------------------------------- приводим boolean к числу----------------------------------- */
+
+    //приводим булевое значения к числу с помощью Number true дает 1
+    let d = Number(true)
     expect(d).toBe(1)
+    // false - 0
+    d = Number(false)
+    expect(d).toBe(0)
+    //приводим булевое значения к числу с помощью + true дает 1 false 0
+    const altD = +false
+    expect(altD).toBe(0)
+    const altaltD = + true
+    expect(altaltD).toBe(1)
+
+
+    const e = Number(undefined)
+    expect(e).toBe(NaN)
+    const i = Number(null)
+    expect(i).toBe(0)
+
+
 
 })
 
-test("parseResult should be correct",()=>{
+test("isNan should be correct",()=>{
+
+    const a = Number.isNaN('text')
+    const b = Number.isNaN(0)
+    const c = Number.isNaN('666')
+    const d = Number.isNaN(true)
+
 
 })
 export default {}
